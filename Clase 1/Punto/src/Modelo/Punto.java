e25cb05faed541fc5b11d9c1317f660d67043283
fedestf <fede520@live.com>
2019-08-17 23:06:34 -0300

    avances

--- Clase 1/Punto/src/Modelo/Punto.java
+++ Clase 1/Punto/src/Modelo/Punto.java
@@ -1,5 +1,44 @@
 package Modelo;
-
+import java.math.*;
 public class Punto {
+	//atributos
+	private double x;
+	private double y;
+
+//constructor
+	public Punto(double x, double y) {
+			this.x = x;
+			this.y = y;
+} 
+// métodos getter y setter
+	public double getX() {
+		return x;
+} 
+	public void setX(double x) {
+		this.x = x;
+} 
+public double getY() {
+return y;
+} 
+	public void setY(double y) {
+		this.y = y;
+} //re-definición de métodos de la clase Object
+// sobrecarga
+	public boolean equals(Punto p){
+		return ((x==p.getX())&&(y==p.getY()));
+} //re-definición
+@Override
+	public String toString(){
+	return "("+x+","+y+")";
+}
+	public double calcularDistancia(Punto p) {
+	
+		return (double)(Math.sqrt(Math.pow(p.getX()-x, 2)+(Math.pow(p.getY()-y,2))));
+	}
+	
+	public void mover(double desplazamientoX,double desplazamientoY) {
+		x=x+desplazamientoX;
+		y=y+desplazamientoY;
+	}
 
 }
