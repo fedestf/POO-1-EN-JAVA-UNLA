1fa90c651c96f54f2b1c373ce13d78d3e0428871
fedestf <fede520@live.com>
2019-08-28 10:04:35 -0300

    --

--- Clase 1/Punto/src/Modelo/Circulo.java
+++ Clase 1/Punto/src/Modelo/Circulo.java
@@ -1,76 +1,71 @@
 package Modelo;
+
 import java.math.*;
 
 public class Circulo {
-	
+
 	private Punto origen;
 	private double radio;
-	
-	
+
 	public Circulo(Punto origen, double radio) {
 		super();
 		this.origen = origen;
 		this.radio = radio;
 	}
 
-
 	public Punto getOrigen() {
 		return origen;
 	}
-
 
 	public void setOrigen(Punto origen) {
 		this.origen = origen;
 	}
 
-
 	public double getRadio() {
 		return radio;
 	}
 
-
 	public void setRadio(double radio) {
 		this.radio = radio;
 	}
-	
-	public void mover (double desplazamientoX,double desplazamientoY) {
+
+	public void mover(double desplazamientoX, double desplazamientoY) {
 		// preguntar este metodo la explicacion
-		Punto nuevoOrigen= new Punto(origen.getX(),origen.getY());
-		origen=nuevoOrigen;
+		Punto nuevoOrigen = new Punto(origen.getX(), origen.getY());
+		origen = nuevoOrigen;
 		origen.mover(desplazamientoX, desplazamientoY);
 	}
-	//preguntar sobre este metodo como se hace
-//	public void setRadio(Punto radio) {
-	//	double Punto=this.radio;
-		
-	//}
-	
+	public void setRadio(Punto radio) {
+	 
+
+	}
+
 	public boolean equals(Circulo c) {
-	
-		return (origen==c.getOrigen()&&radio==c.getRadio());
+
+		return (origen == c.getOrigen() && radio == c.getRadio());
 	}
 
 	// tostring autogenerado?
-	@Override 
+	@Override
 	public String toString() {
 		return "Circulo [origen=" + origen + ", radio=" + radio + "]";
 	}
-	
+
 	public double calcularPerimetro() {
-		
-		return (2*Math.PI*radio);
+
+		return (2 * Math.PI * radio);
 	}
-	
+
 	public double calcularArea() {
-	
-		return (Math.PI*Math.pow(radio, 2));
+
+		return (Math.PI * Math.pow(radio, 2));
 	}
-	
-	public double calcularDistancia (Circulo c) {
-		
-		Punto c2 = new Punto(origen.getX(),origen.getY());
-		
-		return (Math.sqrt(Math.pow(c2.getX()-c.origen.getX(),2)+(Math.pow(c2.getY()-c.origen.getY(),2))));
+
+	public double calcularDistancia(Circulo c) {
+
+		Punto c2 = new Punto(origen.getX(), origen.getY());
+
+		return (Math.sqrt(Math.pow(c2.getX() - c.origen.getX(), 2) + (Math.pow(c2.getY() - c.origen.getY(), 2))));
 	}
 
 }
